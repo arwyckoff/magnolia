a43ff5cf8ed44d79bdf9a6566f2a70d2864bdca6
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-14 17:29:36 -0400

    Returns all plants from DB

---
+++ Application/src/Application.Web/Models/Api/FamilyViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Web.Models.Api
+{
+    public class PlantsFamilyViewModel
+    {
+        public string CommonName { get; set; }
+        public string LatinName { get; set; }
+    }
+}
