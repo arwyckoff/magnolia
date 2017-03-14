f70712f391d4ff1e5106953c14d1b4a594efeca3
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-14 16:10:36 -0400

    Fast-forward to database w/ auth + seeder

---
+++ Application/src/Application.Web/Models/Context/Seeder/TreeSeeder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magnolia.Web.Models.Context.Seeder
+{
+    public class TreeSeeder
+    {
+        public List<string> Characteristics { get; set; }
+        public string CommonName { get; set; }
+        public string LatinName { get; set; }
+        public string FamilyName { get; set; }
+        public string ImageRef { get; set; }
+    }
+}
