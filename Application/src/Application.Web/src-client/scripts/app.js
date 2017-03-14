05530c16d8750b45e8848b246f341c6c0d18b42a
Andrew Wyckoff <andrewwyckoff@andrew-wyckoffs-macbook-2.local>
2017-03-14 16:02:33 -0400

    giving a more user-friendly starter msg

--- Application/src/Application.Web/src-client/scripts/app.js
+++ Application/src/Application.Web/src-client/scripts/app.js
@@ -6,7 +6,7 @@ const SomeComponent = React.createClass({
 	render: function(){
 		return (
 			<div>
-				<h1> I &#10084; u<br/> Baby Shampu</h1>
+				<h1> I Really &#10084; u<br/> Baybee Shampoo</h1>
 				<p><small>
 					you make my hair so soft and i know you will never make me cry.
 				</small></p>
