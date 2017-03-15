a41ef71264b39ead9b04dce3b69467ab9d11a11e
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-15 17:12:31 -0400

    Swaps family in favor of more specific genus

--- Application/src/Application.Web/Models/Context/Plant.cs
+++ Application/src/Application.Web/Models/Context/Plant.cs
@@ -8,11 +8,15 @@ namespace Magnolia.Models
     {
         public int Id { get; set; }
         public string CommonName { get; set; }
+        public string SecondaryName { get; set; }
+        public string TertiaryName { get; set; }
         public string LatinName { get; set; }
         public string Description { get; set; }
         public string ImageRef { get; set; }
+        public int GenusId { get; set; }
         public int FamilyId { get; set; }
 
+        public Genus Genus { get; set; }
         public Family Family { get; set; }
         public List<PlantCharacteristics> PlantCharacteristics { get; set; }
 
