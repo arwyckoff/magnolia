ae663aa81a6942386d8af4abf0795fc7ad8adb86
Mary Hart <maryhart@mhcomp.local>
2017-03-16 10:26:53 -0400

    adding folders

---
+++ Application/src/Application.Web/src-client/scripts/store.js
@@ -0,0 +1,32 @@
+export const STORE = {
+  _data:{
+    treeListData: [],
+    wikiData: '',
+  },
+  getStoreData: function(){
+		return this._data
+	},
+
+	setStore: function(storeProp, payload){
+    if (typeof this._data[storeProp] === 'undefined' ) {
+      throw new Error('cannot pass undefined property to .setStore() ')
+      		}
+	   this._data[storeProp] = payload
+    if (typeof this._callMeLaterPls === 'function'){
+		this.callBackFunc()
+    }
+	},
+
+	onStoreChange: function(cbFunc){
+    if(typeof cbFunc !== 'function'){
+  throw new Error('argument passed to STORE.onStoreChange() must be a function')
+}
+
+if(typeof this.callBackFunc !== 'undefined'){
+  throw new Error('STORE.onStoreChange() already has a function to handle changes')
+}
+		this.callBackFunc = cbFunc
+	}
+
+
+}
