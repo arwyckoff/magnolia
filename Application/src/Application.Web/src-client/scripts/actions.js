1ad6521e1825495c392a3ef69f4c33b401da5e9e
Mary Hart <maryhart@mhcomp.local>
2017-03-16 14:19:52 -0400

    trees

--- Application/src/Application.Web/src-client/scripts/actions.js
+++ Application/src/Application.Web/src-client/scripts/actions.js
@@ -1,8 +1,6 @@
 import Backbone from 'backbone'
-<<<<<<< HEAD
 import {TreeModel, TreeCollection} from './models/tree-model.js'
-=======
->>>>>>> ed91038030a5233f933017b8e888f85de9aaed19
+
 import {STORE} from './store.js'
 // import {PlantViewModel} from '../../Models/Api/PlantViewModel.cs'
 
