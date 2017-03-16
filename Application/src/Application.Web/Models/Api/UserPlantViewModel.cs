203ea3bcd320cbc2512bdefee9eb941fd94923b7
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-16 16:03:04 -0400

    Adds users saving specific plants with comments

---
+++ Application/src/Application.Web/Models/Api/UserPlantViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Application.Web.Models.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magnolia.Web.Models.Api
+{
+    public class UserPlantViewModel
+    {
+        public PlantViewModel Plant { get; set; }
+        public string Comment { get; set; }
+    }
+}
