dc6bbfd66cef670cc275427ce54af355f6609b34
Andrew Wyckoff <andrewrwyckoff@gmail.com>
2017-03-17 14:19:05 -0400

    Add browse view w/ relevant components

--- Application/src/Application.Web/src-client/scripts/app.js
+++ Application/src/Application.Web/src-client/scripts/app.js
@@ -1,11 +1,16 @@
 import Backbone from 'backbone';
 import ReactDOM from 'react-dom'
 import React from 'react'
-
 import {TreeCollection} from './models/tree-model.js'
 import {HomeView} from './views/home-view.js'
 import ViewController from './viewController.js'
 import {BrowseView} from './views/browse-view.js'
+
+if(window.location.hostname === 'localhost'){
+    let headEl = document.querySelector('head')
+    let linkEl = document.querySelector('link[href="./css/styles.css"]')
+    headEl.removeChild(linkEl)
+}
 
 
 const AppRouter = Backbone.Router.extend({
@@ -18,8 +23,8 @@ const AppRouter = Backbone.Router.extend({
 },
 
 showHomePage: function(){
-	console.log('mISter HomeView')
-	ReactDOM.render(<HomeView />, document.querySelector('#app-container'))
+	// console.log('mISter HomeView')
+	ReactDOM.render(<BrowseView />, document.querySelector('#app-container'))
 
 }
 })
