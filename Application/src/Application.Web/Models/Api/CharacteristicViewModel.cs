aa4722e075585ec326a3e756ee8a7a6ca3adbf7f
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-19 15:25:01 -0400

    Add routes to return characteristics by name

--- Application/src/Application.Web/Models/Api/CharacteristicViewModel.cs
+++ Application/src/Application.Web/Models/Api/CharacteristicViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Application.Web.Models.Api;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,14 @@ namespace Magnolia.Web.Models.Api
 {
     public class CharacteristicViewModel
     {
+        public int Id { get; set; }
+        public string Characteristic { get; set; }
+        public int Permutations { get; set; }
+        public List<StateViewModel> States { get; set; }
+
+        public CharacteristicViewModel()
+        {
+            States = new List<StateViewModel>();
+        }
     }
 }
