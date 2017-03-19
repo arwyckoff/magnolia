b0e77d7b50dd870f23f26d44157d0caf8ef2d42c
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-19 15:25:01 -0400

    Add routes to return characteristics by name

--- Application/src/Application.Web/Controllers/CharacteristicsController.cs
+++ Application/src/Application.Web/Controllers/CharacteristicsController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Web.Models.Api;
 using Magnolia.Models;
+using Magnolia.Web.Models.Api;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -29,6 +30,103 @@ namespace Magnolia.Web.Controllers
 
             foreach (var state in states)
             {
+                if (!characteristicViewModels.Keys.Any(k => k == state.Characteristic.Value))
+                {
+                    var c = new CharacteristicViewModel()
+                    {
+                        Id = state.Characteristic.Id,
+                        Characteristic = state.Characteristic.Value,
+                        Permutations = state.Characteristic.Permutations
+                    };
+                    c.States.Add(new StateViewModel()
+                    {
+                        Characteristic = state.Characteristic.Value,
+                        State = state.Value,
+                        Code = state.Code
+                    });
+
+                    characteristicViewModels.Add(c.Characteristic, c);
+                }
+                else
+                {
+                    var c = characteristicViewModels[state.Characteristic.Value];
+                    c.States.Add(new StateViewModel()
+                    {
+                        Characteristic = c.Characteristic,
+                        State = state.Value,
+                        Code = state.Code
+                    });
+                }
+            }
+
+            return Ok(characteristicViewModels);
+        }
+
+        [Route("~/api/characteristics/byid/{id}")]
+        [HttpGet]
+        public async Task<IActionResult> Get(int id)
+        {
+            var characteristic = await _context.Characteristics.Include(c => c.States)
+                                                               .FirstOrDefaultAsync(c => c.Id == id);
+
+            if (characteristic == null)
+            {
+                return NotFound(id);
+            }
+
+            var characteristicViewModel = new CharacteristicViewModel()
+            {
+                Id = characteristic.Id,
+                Characteristic = characteristic.Value,
+                Permutations = characteristic.Permutations
+            };
+
+            foreach (var state in characteristic.States)
+            {
+                characteristicViewModel.States.Add(new StateViewModel()
+                {
+                    Characteristic = characteristicViewModel.Characteristic,
+                    State = state.Value,
+                    Code = state.Code
+                });
+            }
+
+            return Ok(characteristicViewModel);
+        }
+
+        [Route("~/api/characteristics/byname/{value}")]
+        [HttpGet]
+        public async Task<IActionResult> Get(string value)
+        {
+            var v = value.ToLower();
+            var characteristic = await _context.Characteristics.Include(c => c.States)
+                                                               .FirstOrDefaultAsync(c => c.Value == v);
+
+            if (characteristic == null)
+            {
+                return NotFound(value);
+            }
+
+            var characteristicViewModel = new CharacteristicViewModel()
+            {
+                Id = characteristic.Id,
+                Characteristic = characteristic.Value,
+                Permutations = characteristic.Permutations
+            };
+
+            foreach (var state in characteristic.States)
+            {
+                characteristicViewModel.States.Add(new StateViewModel()
+                {
+                    Characteristic = characteristicViewModel.Characteristic,
+                    State = state.Value,
+                    Code = state.Code
+                });
+            }
+
+            return Ok(characteristicViewModel);
+        }
+
         [Route("~/api/characteristics/codes")]
         [HttpGet]
         public async Task<IActionResult> GetCodes()
