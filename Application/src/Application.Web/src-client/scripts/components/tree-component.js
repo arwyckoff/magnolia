9ee0d5b738bdd78a7ff1824bfdb5b8b5743c1e84
Andrew Wyckoff <andrewrwyckoff@gmail.com>
2017-03-20 11:22:49 -0400

    updating to resolve merge conflict

--- Application/src/Application.Web/src-client/scripts/components/tree-component.js
+++ Application/src/Application.Web/src-client/scripts/components/tree-component.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {TreeModel, TreeCollection} from '../models/tree-model.js'
 import {STORE} from '../store.js'
 import {ACTIONS} from '../actions.js'
+import {BROWSE_ACTIONS} from '../browse_actions.js'
+
 export const TreeListComponent = React.createClass({
   _makeTreeComponents: function(treeList){
    let arrayOfTreeComponents = treeList.map(function(smod, i){
@@ -33,22 +35,26 @@ export const TreeItem = React.createClass({
     ACTIONS.changeCurrentNav ('PROFILE', `${id}/${latinName}`)
   },
    render: function(){
-     let statesArray = []
-     let treeDataCommonNames = this.props.treeData.commonName
+    //  let statesArray = []
+    //  let treeDataCommonNames = this.props.treeData.commonName
+     //
+    //  if(statesArray.indexOf(this.props.filterChars) === -1){
+    //    statesArray.push(treeDataCommonNames)
+    //    console.log(statesArray)
+    //  }
 
-     if(statesArray.indexOf(this.props.filterChars) === -1){
-       statesArray.push(treeDataCommonNames)
-       console.log(statesArray)
-     }
+
       return (
 
         <div className = "container-tree">
-                   <p className= "single-tree">{this.props.treeData.commonName}</p>
+                   {/* <p className= "single-tree">{this.props.treeData.commonName}</p> */}
+                <h1>ehlllloo</h1>
                     {/* <p className= "single-tree prop">{statesArray}</p> */}
-        <div className = "container-tree" onClick = {this._handleProfClick} data-route = {this.props.treeData.id} data-id = {this.props.treeData.latinName}>
-          <h4>this tree</h4>
-                   <p>{this.props.treeData.commonName}</p>
-         </div></div>
+        {/* <div className = "container-tree" onClick = {this._handleProfClick} data-route = {this.props.treeData.id} data-id = {this.props.treeData.latinName}>
+   */}
+                   {/* <p>{this.props.treeData.commonName}</p> */}
+         {/* </div> */}
+       </div>
       )
    }
 })
