5ba866edef04508558f14447a91fb87aceec51b7
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-20 16:16:13 -0400

    Add handling weird dependency notations

--- Application/src/Application.Web/src-client/scripts/utils/getFilteredCharacteristics.js
+++ Application/src/Application.Web/src-client/scripts/utils/getFilteredCharacteristics.js
@@ -4,10 +4,26 @@ const _getFilteredCharacteristics = (filters, allCharacteristics) => {
         let characteristic = allCharacteristics[c];
         if (characteristic.depends === "") {
             filteredCharacteristics.push(characteristic);
+        } else if (characteristic.depends.length === 1) {
+            for (var f = 0, fLen = filters.length; f < fLen; f++) {
+                let filterPrefix = filters[f][0];
+                if (characteristic.depends === filterPrefix) {
+                    filteredCharacteristics.push(characteristic);
+                    break;
+                }
+            }
+        } else if (characteristic.depends === characteristic.depends.toUpperCase()) {
+            for (var f = 0, fLen = filters.length; f < fLen; f++) {
+                let filterPrefix = filters[f].slice(0, 2);
+                if (characteristic.depends === filterPrefix) {
+                    filteredCharacteristics.push(characteristic);
+                    break;
+                }
+            }
         } else {
             for (var f = 0, fLen = filters.length; f < fLen; f++) {
                 let filter = filters[f];
-                if (characteristic.depends === f) {
+                if (characteristic.depends === filter) {
                     filteredCharacteristics.push(characteristic);
                     break;
                 }
