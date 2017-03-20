420d90fca9beb049f386b6ce4925a4a2e05e4da5
Andrew Wyckoff <andrewrwyckoff@gmail.com>
2017-03-20 12:32:07 -0400

    Fix property misspellings

--- Application/src/Application.Web/src-client/scripts/utils/getFilteredTrees.js
+++ Application/src/Application.Web/src-client/scripts/utils/getFilteredTrees.js
@@ -1,12 +1,12 @@
 const _getFilteredTrees = (filters, allTrees) => {
     let filteredTrees = [];
     let filterLength = filters.length;
-    for (let t = 0, tLen = allTrees.legth; t < tLen; t++) {
+    for (let t = 0, tLen = allTrees.length; t < tLen; t++) {
         let tree = allTrees[t];
         let intersection = 0;
         for (let f = 0, fLen = filterLength; f < fLen; f++) {
             let filter = filters[f];
-            if (tree.characteristicHash[filter] !== undefined) {
+            if (tree.characteristicsHash[filter] !== undefined) {
                 intersection++;
             }
         }
