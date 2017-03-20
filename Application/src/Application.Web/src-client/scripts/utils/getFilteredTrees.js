1d50f78528b399ca75f4677ec67d05860bc90310
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-20 12:14:37 -0400

    Add function to filter trees

---
+++ Application/src/Application.Web/src-client/scripts/utils/getFilteredTrees.js
@@ -0,0 +1,20 @@
+const _getFilteredTrees = (filters, allTrees) => {
+    let filteredTrees = [];
+    let filterLength = filters.length;
+    for (let t = 0, tLen = allTrees.legth; t < tLen; t++) {
+        let tree = allTrees[t];
+        let intersection = 0;
+        for (let f = 0; fLen = filterLength; f < fLen; f++) {
+            let filter = filters[f];
+            if (tree.characteristicHash[filter] !== undefined) {
+                intersection++;
+            }
+        }
+        if (intersection == filterLength) {
+            filteredTrees.push(tree);
+        }
+    }
+    return filteredTrees;
+}
+
+export default _getFilteredTrees;
