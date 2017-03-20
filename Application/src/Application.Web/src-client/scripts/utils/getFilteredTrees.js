27caa6f1fe7dc9f0cc07628f93c592f21ba7a3dc
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-20 12:15:20 -0400

    Fix semicolon

--- Application/src/Application.Web/src-client/scripts/utils/getFilteredTrees.js
+++ Application/src/Application.Web/src-client/scripts/utils/getFilteredTrees.js
@@ -4,7 +4,7 @@ const _getFilteredTrees = (filters, allTrees) => {
     for (let t = 0, tLen = allTrees.legth; t < tLen; t++) {
         let tree = allTrees[t];
         let intersection = 0;
-        for (let f = 0; fLen = filterLength; f < fLen; f++) {
+        for (let f = 0, fLen = filterLength; f < fLen; f++) {
             let filter = filters[f];
             if (tree.characteristicHash[filter] !== undefined) {
                 intersection++;
