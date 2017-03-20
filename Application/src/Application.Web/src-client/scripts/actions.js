8c4b25f8b084899eac512f1409c4effa4e43cb2d
Mary Hart <marymhart@gmail.com>
2017-03-20 10:25:16 -0400

    updating model refs

--- Application/src/Application.Web/src-client/scripts/actions.js
+++ Application/src/Application.Web/src-client/scripts/actions.js
@@ -1,7 +1,6 @@
 import Backbone from 'backbone'
 import {TreeModel, TreeCollection} from './models/tree-model.js'
 import {WikiModel, WikiCollection} from './models/wiki-model.js'
-import {ImageModel, ImageCollection} from './models/image-model.js'
 import {UserModel} from './models/model-user.js'
 import {TreeNameModel} from './models/tree-name-model.js'
 
