a361419ae3e76613422027285d8f938bc8ac5d20
Mary Hart <marymhart@gmail.com>
2017-03-20 10:16:43 -0400

    working profile pages with wiki

---
+++ Application/src/Application.Web/src-client/scripts/components/landing-component.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import {STORE} from '../store.js'
+import {ACTIONS} from '../actions.js'
+
+export const LandingComponent = React.createClass({
+
+   render: function(){
+    //  console.log(this.props.filterChars)
+
+      return (
+
+        <img src = '../../2.eps'/>
+        <div className = "landing-container">
+          <h1>Magnolia Tree Id</h1>
+         </div>
+      )
+   }
+})
