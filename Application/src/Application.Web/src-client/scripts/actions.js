b152e7d10885e074d74ba461d2bde7a7f8819a36
Andrew Wyckoff <andrewrwyckoff@gmail.com>
2017-03-21 13:20:47 -0400

    updated actions and tree-componenet.js

--- Application/src/Application.Web/src-client/scripts/actions.js
+++ Application/src/Application.Web/src-client/scripts/actions.js
@@ -13,6 +13,7 @@ fetchAllTrees: function(){
   let TreeCollInstance = new TreeCollection()
   TreeCollInstance.fetch().then(function(serverRes){
     STORE.setStore('treeListData', serverRes)
+    STORE.setStore('filteredTrees', serverRes)
   })
   },
 
