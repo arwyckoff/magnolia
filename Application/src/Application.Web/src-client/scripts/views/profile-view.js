04334d095cc97906f1b2944d05363230925191d2
Mary Hart <marymhart@gmail.com>
2017-03-21 18:17:29 -0400

    adding genus sibs to profile page

--- Application/src/Application.Web/src-client/scripts/views/profile-view.js
+++ Application/src/Application.Web/src-client/scripts/views/profile-view.js
@@ -2,7 +2,7 @@ import Backbone from 'backbone'
 import React from 'react'
 import {ACTIONS} from '../actions.js'
 import {STORE} from '../store.js'
-import {ProfileComponent} from '../components/profile-component.js'
+import {GenusComponent, ProfileComponent} from '../components/profile-component.js'
 
 
 export const ProfileView = React.createClass({
@@ -13,15 +13,22 @@ export const ProfileView = React.createClass({
 
   componentDidMount: function(){
     let component = this
-    console.log(this.state)
+    let latinName = window.location.hash.slice(6)
+    let latinGenus = latinName.split(' ')
+    let latinGenusWord = latinGenus[0]
+    STORE.setStore('genus', latinGenusWord)
+    console.log(this.state.genus)
+    ACTIONS.fetchGenusTrees(this.state.genus)
     ACTIONS.fetchMyLatinTree(window.location.hash.slice(6))
     ACTIONS.fetchMyWiki(window.location.hash.slice(6))
+
   },
 
   render: function(){
     return(
-      <div className = "container">
+      <div className = "profile-container">
         <ProfileComponent {...this.state}/>
+        <GenusComponent {...this.state}/>
       </div>
     )
   }
