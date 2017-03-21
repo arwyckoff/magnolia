d3cf336bf0f9afd3cf91a937c70f842431051322
Mary Hart <marymhart@gmail.com>
2017-03-21 18:17:29 -0400

    adding genus sibs to profile page

--- Application/src/Application.Web/src-client/scripts/components/profile-component.js
+++ Application/src/Application.Web/src-client/scripts/components/profile-component.js
@@ -10,7 +10,9 @@ export const ProfileComponent = React.createClass({
    render: function(){
       let self = this
       let allTheTrees = this.props.myTree
+
       return (
+        <div className = "profile-page">
         <div className = "tree-profile">
            <ProfileItem profileData={allTheTrees}/>
            <div className = "profile-half">
@@ -21,6 +23,7 @@ export const ProfileComponent = React.createClass({
             </div>
 
         </div>
+      </div>
       )
    }
 })
@@ -40,5 +43,53 @@ export const ProfileItem = React.createClass({
        </div>
 
      )
+   }
+})
+
+
+
+export const GenusComponent = React.createClass({
+
+  componentDidMount: function (){
+    let self = this
+  },
+
+  _makeTreeComponents: function(treeList){
+   let arrayOfTreeComponents = treeList.map(function(smod, i){
+        return (
+           <GenusItem treeData={smod} key={i}/>
+        )
+     })
+   return arrayOfTreeComponents
+ },
+
+   render: function(){
+      return (
+        <div className = "genus-container">
+          <h4> Other trees in Genus <em>{this.props.genus}</em></h4>
+               {this._makeTreeComponents(this.props.genusTrees)}
+        </div>
+      )
+    }
+})
+
+export const GenusItem = React.createClass({
+  _handleProfClick: function(evt){
+    evt.preventDefault()
+    let profileEl = evt.currentTarget
+    let id = this.props.treeData.id
+    let latinName = this.props.treeData.latinName
+    ACTIONS.changeCurrentNav ('PROFILE', latinName)
+  },
+   render: function(){
+
+      return (
+
+        <div className = "genus-box" onClick = {this._handleProfClick}>
+          <img src = "http://placehold.it/200?text=add+wiki+image"/>
+              <p className= "single-tree">{this.props.treeData.commonName}</p>
+              <p><em>{this.props.treeData.latinName}</em></p>
+       </div>
+      )
    }
 })
