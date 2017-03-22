7df760d2f39a2e052b8b85ae1b4867d8192ade79
Mary Hart <marymhart@gmail.com>
2017-03-22 14:37:46 -0400

    home list update

--- Application/src/Application.Web/src-client/scripts/components/tree-profile-component.js
+++ Application/src/Application.Web/src-client/scripts/components/tree-profile-component.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {TreeModel, TreeCollection} from '../models/tree-model.js'
 import {STORE} from '../store.js'
 import {ACTIONS} from '../actions.js'
+
 export const TreeProfileComponent = React.createClass({
   _makeTreeComponents: function(treeList){
    let arrayOfTreeComponents = treeList.map(function(smod, i){
@@ -32,19 +33,12 @@ export const TreeItem = React.createClass({
     ACTIONS.changeCurrentNav ('PROFILE', latinRoute)
   },
    render: function(){
-    //  let statesArray = []
-    //  let treeDataCommonNames = this.props.treeData.commonName
-     //
-    //  if(statesArray.indexOf(this.props.filterChars) === -1){
-    //    statesArray.push(treeDataCommonNames)
-    //    console.log(statesArray)
-    //  }
+
       return (
 
-        <div className = "container-tree">
-                   <p className= "single-tree">{this.props.treeData.commonName}</p>
+        <div className = "container-tree homw-tree" onClick = {this._handleProfClick} >
                     {/* <p className= "single-tree prop">{statesArray}</p> */}
-        <div className = "container-tree" onClick = {this._handleProfClick} data-route = {this.props.treeData.id} data-id = {this.props.treeData.latinName}>
+        <div className = "container-tree" data-route = {this.props.treeData.id} data-id = {this.props.treeData.latinName}>
           <h4>this tree</h4>
                    <p>{this.props.treeData.commonName}</p>
          </div></div>
