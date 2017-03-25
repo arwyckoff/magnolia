df9fdec21af487c26fd394d4583acfe90d206d4d
Mary Hart <marymhart@gmail.com>
2017-03-24 23:21:52 -0400

    navbar full length & other formatting

--- Application/src/Application.Web/src-client/scripts/views/landing-view.js
+++ Application/src/Application.Web/src-client/scripts/views/landing-view.js
@@ -12,13 +12,13 @@ export const LandingView = React.createClass({
   render: function () {
     return (
       <div className="hero">
-        <img className="hero-img" src='http://i64.tinypic.com/2lthudd.jpg' />
+        <img className="hero-img" src='../../images/mag-hero.jpg' />
         <div className="landing-container">             <h1>Magnolia TreeID</h1>
           <div className="landing-options">
-            <button className="button-nav"><i className="fa fa-search fa-2x" aria-hidden="true"></i><span className="btn-description">IDentify</span></button>
+            {/* <button className="button-nav"><i className="fa fa-search fa-2x" aria-hidden="true"></i><span className="btn-description">IDentify</span></button>
             <button className="button-nav"><i className="fa fa-book fa-2x" aria-hidden="true"></i><span className="btn-description">Browse all plants</span></button>
             <button className="button-login">Login</button>
-            <button className="button-login">Register</button>
+            <button className="button-login">Register</button> */}
           </div>
         </div>
       </div>
