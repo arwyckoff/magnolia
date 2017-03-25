4004598c08fa50b4bb48e00d199b1037e76c7e30
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-25 15:52:55 -0400

    Add indentification question models

---
+++ Application/src/Application.Web/Models/Context/QuestionAnswer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magnolia.Context.Models
+{
+    public class QuestionAnswer
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+        
+        public List<State> ResultingStates { get; set; }
+        public Question Question { get; set; }
+
+        public QuestionAnswer()
+        {
+            ResultingStates = new List<State>();
+        }
+    }
+}
