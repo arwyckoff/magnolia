6074413467ae9d9e3d2ab328bcb86cbbeec6e284
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-25 15:52:55 -0400

    Add indentification question models

---
+++ Application/src/Application.Web/Models/Context/QuestionOrders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magnolia.Context.Models
+{
+    public struct QuestionOrder
+    {
+        public const string Category = "CATEGORY";
+        public const string SubCategory = "SUBCATEGORY";
+        public const string Finalizer = "FINALIZER";
+    }
+}
