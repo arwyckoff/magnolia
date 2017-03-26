d29e7b9b9ae7d327de8d785f600a0d1bdb1d6aad
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-26 18:09:41 -0400

    Add caching plants server-side

---
+++ Application/src/Application.Web/Models/Context/Cache.cs
@@ -0,0 +1,62 @@
+﻿using Magnolia.Context.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magnolia.Api.Models
+{
+    public class Cache
+    {
+        private static List<PlantViewModel> _plantViewModels;
+
+        private Cache() { }
+
+        public static async Task<List<PlantViewModel>> GetPlantViewModels(MagnoliaContext context)
+        {
+            if (_plantViewModels == null)
+            {
+                var plants = await context.Plants.Include(p => p.PlantCharacteristics)
+                                              .Include(p => p.Family)
+                                              .ToListAsync();
+
+                var plantViewModels = new List<PlantViewModel>();
+
+                foreach (var plant in plants)
+                {
+                    var plantViewModel = new PlantViewModel()
+                    {
+                        Id = plant.Id,
+                        CommonName = plant.CommonName,
+                        SecondaryName = plant.SecondaryName ?? "",
+                        TertiaryName = plant.TertiaryName ?? "",
+                        LatinName = plant.LatinName,
+                        Family = new PlantsFamilyViewModel()
+                        {
+                            CommonName = plant.Family.CommonName,
+                            LatinName = plant.Family.LatinName
+                        }
+                    };
+
+                    foreach (var characteristic in plant.PlantCharacteristics)
+                    {
+                        var state = await context.States.Include(s => s.Characteristic)
+                                                         .FirstOrDefaultAsync(s => s.Id == characteristic.StateId);
+
+                        if (plantViewModel.Characteristics.Keys.Any(k => k == state.Code))
+                            continue;
+
+                        plantViewModel.Characteristics.Add(state.Code, null);
+                    }
+
+                    plantViewModels.Add(plantViewModel);
+                }
+
+                _plantViewModels = plantViewModels;
+            }
+
+            return _plantViewModels;
+        }
+    }
+}
