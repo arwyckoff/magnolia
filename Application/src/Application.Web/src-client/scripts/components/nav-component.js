2bf7f8d6848db318e19427e88f1bdbaf21b23f44
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-28 18:35:12 -0400

    Use new preloader on profile

--- Application/src/Application.Web/src-client/scripts/components/nav-component.js
+++ Application/src/Application.Web/src-client/scripts/components/nav-component.js
@@ -1,45 +1,49 @@
 import React from 'react'
-import {ACTIONS} from '../actions.js'
-import {STORE} from '../store.js'
-import {PreloaderComponent} from '../components/preloader-component.js'
+import { ACTIONS } from '../actions.js'
+import { STORE } from '../store.js'
+import { PreloaderComponent } from '../components/preloader-component.js'
 
 export const NavBar = React.createClass({
-  getInitialState: function() {
+  getInitialState: function () {
     return STORE.getStoreData()
   },
-  _getMenuOptions: function(currentUserOnStore){
-		let routeList
-		if( currentUserOnStore.id === 'undefined' || currentUserOnStore.id === null){
-       routeList = [
-          {appRouteName: 'HOME', displayText: <i className="fa fa-tree fa-2x"><span className="label arrow_box">home</span></i>,  hashRoute: ''},
-          {appRouteName: 'IDENTIFY', displayText: <i className = "fa-2x fa fa-leaf"><span className = "label arrow_box">identify</span></i>, hashRoute: 'identify'},
-          {appRouteName: 'BROWSE', displayText: <i className="fa-2x fa fa-binoculars"><span className="label arrow_box">browse</span></i>, hashRoute: 'browse'},
-          {appRouteName: 'LOGIN', displayText: <i className="fa-2x fa fa-sign-in"><span className="label arrow_box">login</span></i>, hashRoute: 'login'},
-          {appRouteName: 'REGISTER', displayText: <i className="fa-2x fa fa-user-plus"><span className="label arrow_box">register</span></i>, hashRoute: 'register'},
 
-        ]
-      }else {
-    			routeList = [
-    				{appRouteName: 'HOME', displayText: <i className="fa fa-2x fa-tree"><span className="label arrow_box">home</span></i>,  hashRoute:  '' },
-            {appRouteName: 'IDENTIFY', displayText: <i className = "fa-2x fa fa-search"><span className = "label arrow_box">identify</span></i>, hashRoute: 'identify'},
-    	      {appRouteName: 'BROWSE', displayText:  <i className="fa-2x fa fa-binoculars" aria-hidden="true"><span className="label arrow_box">browse</span></i>, hashRoute: 'browse'},
-            {appRouteName: 'MYPROFILE', displayText:<i className="fa-2x fa fa-user-o" aria-hidden="true"><span className = "label arrow_box">my profile</span></i>, hashRoute: 'my-profile' },
-            {appRouteName: 'LOGOUT', displayText:<i className="fa-2x fa fa-sign-out" aria-hidden="true"><span className="label arrow_box">logout</span></i>, hashRoute: 'logout' }
-    			]
-    		}
-    		return routeList
-    	},
-  _showNavOptionsJSX: function(currentNavRoute, currentUser){
+  _getMenuOptions: function (currentUserOnStore) {
+    let routeList
+    if (currentUserOnStore.id === 'undefined' || currentUserOnStore.id === null) {
+      routeList = [
+        { appRouteName: 'HOME', displayText: <i className="fa fa-tree fa-2x"><span className="label arrow_box">home</span></i>, hashRoute: '' },
+        { appRouteName: 'IDENTIFY', displayText: <i className="fa-2x fa fa-leaf"><span className="label arrow_box">identify</span></i>, hashRoute: 'identify' },
+        { appRouteName: 'BROWSE', displayText: <i className="fa-2x fa fa-binoculars"><span className="label arrow_box">browse</span></i>, hashRoute: 'browse' },
+        { appRouteName: 'LOGIN', displayText: <i className="fa-2x fa fa-sign-in"><span className="label arrow_box">login</span></i>, hashRoute: 'login' },
+        { appRouteName: 'REGISTER', displayText: <i className="fa-2x fa fa-user-plus"><span className="label arrow_box">register</span></i>, hashRoute: 'register' },
+
+      ]
+    } else {
+      routeList = [
+        { appRouteName: 'HOME', displayText: <i className="fa fa-2x fa-tree"><span className="label arrow_box">home</span></i>, hashRoute: '' },
+        { appRouteName: 'IDENTIFY', displayText: <i className="fa-2x fa fa-search"><span className="label arrow_box">identify</span></i>, hashRoute: 'identify' },
+        { appRouteName: 'BROWSE', displayText: <i className="fa-2x fa fa-binoculars" aria-hidden="true"><span className="label arrow_box">browse</span></i>, hashRoute: 'browse' },
+        { appRouteName: 'MYPROFILE', displayText: <i className="fa-2x fa fa-user-o" aria-hidden="true"><span className="label arrow_box">my profile</span></i>, hashRoute: 'my-profile' },
+        { appRouteName: 'LOGOUT', displayText: <i className="fa-2x fa fa-sign-out" aria-hidden="true"><span className="label arrow_box">logout</span></i>, hashRoute: 'logout' }
+      ]
+    }
+    return routeList
+  },
+
+  _showNavOptionsJSX: function (currentNavRoute, currentUser) {
     let theMenuRoutes = this._getMenuOptions(currentUser)
 
+    let componentsList = theMenuRoutes.map(
+      (routeObj, i) => {
+        return <RouteOption {...routeObj} key={i} _currentNavRoute={currentNavRoute} />
+      }
+    )
 
-    let componentsList = theMenuRoutes.map(function(routeObj, i){
-      return <RouteOption {...routeObj} key = {i} _currentNavRoute={currentNavRoute}/>
-    })
     return componentsList
   },
 
-  componentWillMount: function() {
+  componentWillMount: function () {
     if (this.props.currentNavRoute === 'PROFILE') {
       ACTIONS.changeReadyState(false)
     } else {
@@ -47,43 +51,42 @@ export const NavBar = React.createClass({
     }
   },
 
-  render: function(){
+  render: function () {
     if (this.props.ready === false) {
-      return(
-        <PreloaderComponent/>
+      return (
+        <PreloaderComponent />
       )
     } else {
-    return(
-      <nav className = "container navbar navbar-fixed-top navbar-styles">
-        <p className="navbar-text navRoute">MAGNOLIA TREE ID</p>
-        <p className="navbar-text navRoute">{this.props.currentNavRoute}
-        </p>
-       <ul className ="navbar-right">
-        {this._showNavOptionsJSX(this.props.appRouteName, this.props.currentUser)}
-      </ul>
-    </nav>
-    )
+      return (
+        <nav className="navbar-container navbar navbar-fixed-top navbar-styles">
+          <div className="navbar-text navRoute">MAGNOLIA TREE ID</div>
+          <div className="navbar-text navRoute">{this.props.currentNavRoute}</div>
+          <ul className="route-options">
+            {this._showNavOptionsJSX(this.props.appRouteName, this.props.currentUser)}
+          </ul>
+        </nav>
+      )
+    }
   }
-}
 })
 
 
 export const RouteOption = React.createClass({
-  _handleNavClick: function(evt){
+  _handleNavClick: function (evt) {
     ACTIONS.changeCurrentNav(this.props.appRouteName, this.props.hashRoute)
   },
-  render: function(){
+  render: function () {
     let navOptionsClassName = "nav-option hvr-grow"
-    if(this.props.hashRoute === window.location.hash.slice(1)){
+    if (this.props.hashRoute === window.location.hash.slice(1)) {
       navOptionsClassName = 'nav-option nav-option-act hvr-grow'
-  }
-      return(
-        <li
-          onClick = {this._handleNavClick}
-          className = {navOptionsClassName}
-        >
-          {this.props.displayText}
-        </li>
-)
     }
+    return (
+      <li
+        onClick={this._handleNavClick}
+        className={navOptionsClassName}
+      >
+        {this.props.displayText}
+      </li>
+    )
+  }
 })
