d010c8f5d838e02cd6715327adfc1eae165f5ff7
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-28 15:24:30 -0400

    Give browse view a facelift

--- Application/src/Application.Web/src-client/scripts/components/characteristic-component.js
+++ Application/src/Application.Web/src-client/scripts/components/characteristic-component.js
@@ -1,59 +1,47 @@
 import React from 'react'
-import {TreeModel, TreeCollection} from '../models/tree-model.js'
-import {CodeModel, CodeCollection} from '../models/code-model.js'
-import {STORE} from '../store.js'
-import {ACTIONS} from '../actions.js'
-import {BROWSE_ACTIONS} from '../browse_actions.js'
-import {CATEGORIES} from '../utils/categories.js'
+import { TreeModel, TreeCollection } from '../models/tree-model.js'
+import { CodeModel, CodeCollection } from '../models/code-model.js'
+import { STORE } from '../store.js'
+import { ACTIONS } from '../actions.js'
+import { BROWSE_ACTIONS } from '../browse_actions.js'
+import { CATEGORIES } from '../utils/categories.js'
 
 
 export const CharacteristicComponent = React.createClass({
-
-  componentDidMount: function (){
-
-
-
-  },
-
-  _handleCharSelect: function(evt){
-
+  _handleCharSelect: function (evt) {
     let charClicked = evt.currentTarget.dataset.cat
     ACTIONS.changeCharacteristic(charClicked)
-
-
   },
 
-  // _handleHEY:function(evt){
-  //
-  //   console.log("hey")
-  // }
-
-
-render: function (){
-  let self = this
-  if(this.props.categories.BARK === undefined){
-    return(
-      <div></div>
-    )
-  }
+  render: function () {
+    if (this.props.categories.BARK === undefined) {
+      return (
+        <div></div>
+      )
+    }
 
     let characteristics = this.props.categories[this.props.categorySelect]
 
-    let charJSX = characteristics.map(function(obj,i){
-      if(obj.characteristic === self.props.characteristicSelect){
-      return <div className="filter active makeHand hvr-grow" onClick={self._handleCharSelect} data-cat={obj.characteristic} key={i}><a>{obj.characteristic}</a></div>
-    } else {
-      return <div className="filter makeHand hvr-grow" onClick={self._handleCharSelect} data-cat={obj.characteristic} key={i}><a>{obj.characteristic}</a></div>
-            }
+    let charJSX = characteristics.map(
+      (obj, i) => {
+        let trimmed = obj.characteristic.replace(
+          this.props.categorySelect.toLowerCase() + ' ',
+          ''
+        )
+        if (obj.characteristic === this.props.characteristicSelect) {
+          return <div className="filter filter-selected makeHand hvr-grow" onClick={this._handleCharSelect} data-cat={obj.characteristic} key={i}>{trimmed}</div>
+        } else {
+          return <div className="filter makeHand hvr-grow" onClick={this._handleCharSelect} data-cat={obj.characteristic} key={i}>{trimmed}</div>
         }
-      )
+      }
+    )
 
-  return (
+    return (
 
-    <div>
-      <h4>Characteristics</h4>
-      {charJSX}
-    </div>
+      <div>
+        <h4>Characteristics</h4>
+        {charJSX}
+      </div>
 
 
     )
