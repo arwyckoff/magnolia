18f9fc8a88513e226db9d147f2379c6ad7c5bc74
Andrew Wyckoff <andrewrwyckoff@gmail.com>
2017-03-28 18:31:58 -0400

    Cache Questions

--- Application/src/Application.Web/Models/Api/Cache.cs
+++ Application/src/Application.Web/Models/Api/Cache.cs
@@ -9,9 +9,59 @@ namespace Magnolia.Api.Models
 {
     public class Cache
     {
-        public static List<PlantViewModel> _plantViewModels;
+        private static List<PlantViewModel> _plantViewModels;
+        private static Dictionary<string, List<QuestionViewModel>> _questionViewModels;
 
         private Cache() { }
+
+        public async static Task<Dictionary<string, List<QuestionViewModel>>> GetQuestionViewModels(MagnoliaContext context)
+        {
+            if (_questionViewModels != null)
+                return _questionViewModels;
+
+            var questions = await context.Questions.Include(q => q.Answers)
+                                                    .Include(q => q.Depends)
+                                                    .Include(q => q.SkipIf)
+                                                    .ToListAsync();
+
+            var questionViewModels = new Dictionary<string, List<QuestionViewModel>>();
+
+            foreach (var question in questions)
+            {
+                var q = new QuestionViewModel()
+                {
+                    Question = question.Value,
+                    Category = question.Category,
+                    Description = question.Description,
+                    SkipIf = question.SkipIfId == null ? "" : context.QuestionAnswers.FirstOrDefault(a => a.Id == question.SkipIfId).Code,
+                    Depends = question.Depends == null ? "" : context.QuestionAnswers.FirstOrDefault(a => a.Id == question.DependsId).Code
+                };
+
+                foreach (var answer in question.Answers)
+                {
+                    var a = new AnswerViewModel();
+                    a.Answer = answer.Value;
+                    a.Description = answer.Description;
+                    a.Code = answer.Code;
+                    a.Apply = answer.ApplyId == 0 ? "" : context.States.FirstOrDefault(s => s.Id == answer.ApplyId).Code;
+
+                    q.Answers.Add(a);
+                }
+
+                if (questionViewModels.Keys.Any(k => k == q.Category))
+                {
+                    questionViewModels[q.Category].Add(q);
+                }
+                else
+                {
+                    questionViewModels.Add(q.Category, new List<QuestionViewModel>() { q });
+                }
+            }
+
+            _questionViewModels = questionViewModels;
+
+            return _questionViewModels;
+        }
 
         public async static Task<List<PlantViewModel>> GetPlantViewModels(MagnoliaContext context)
         {
