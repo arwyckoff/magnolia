201699baf1431d4285ce903cba84f59bf6b65eb6
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-28 22:51:02 -0400

    Make landing more expressive

--- Application/src/Application.Web/src-client/scripts/components/landing-component.js
+++ Application/src/Application.Web/src-client/scripts/components/landing-component.js
@@ -6,7 +6,9 @@ export const LandingComponent = React.createClass({
   render: function () {
     return (
       <div className="landing-container">
-        <h1>M<span className="smallcaps">AGNOLIA</span> T<span className="smallcaps">REE</span> ID</h1>
+        <h1 className="landing-title">M<span className="smallcaps">AGNOLIA</span> T<span className="smallcaps">REE</span> ID</h1>
+        <div className="landing-line"></div>
+        <div className="landing-subtitle">A polychotomous tree identification engine</div>
       </div>
     )
   }
