b164e212a52913908745e17986b7cdcc949efe5b
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-28 22:30:16 -0400

    Add bg svg to all views

--- Application/src/Application.Web/src-client/scripts/components/login-component.js
+++ Application/src/Application.Web/src-client/scripts/components/login-component.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import {ACTIONS} from '../actions.js'
-import {UserModel} from '../models/model-user.js'
+import { ACTIONS } from '../actions.js'
+import { UserModel } from '../models/model-user.js'
 
 export const LoginComponent = React.createClass({
 
-  _handleLogin: function(evt){
+  _handleLogin: function (evt) {
     evt.preventDefault()
     let formEl = evt.target
     let email = formEl.emailField.value
@@ -12,29 +12,26 @@ export const LoginComponent = React.createClass({
     ACTIONS.loginUser(email, password)
   },
 
-  render: function(){
-      return (
-        <div className = "hero">
-
-                <img className = "hero-img" src = '../../images/jon-flobrant-42709.jpg'/>
-
-            <div className="auth-form login-box">
- 				       <form onSubmit = {this._handleLogin}>
-                    <div className = "group">
- 					            <input type="text" className="form-control" name="emailField" placeholder="Email"/>
-                          <span className="highlight"></span>
-                          <span className="bar"></span>
-                      </div>
-                      <div className = "group">
- 	                    <input type="password" className="form-control" name="passwordField" placeholder= "Password"/>
-                          <span className="highlight"></span>
-                          <span className="bar"></span>
-                      </div>
-                 <button type="submit" className = "loginbut"><img src = "../../images/tree-576846_1280.png" className = "loginlog"/><span className="label loginarrow_box">LOG in</span></button>
- 				       </form>
-			     </div>
-         </div>
-
-      )
-   }
+  render: function () {
+    return (
+      <div className="hero">
+        <div className="auth-form login-box">
+          <h2>login</h2>
+          <form onSubmit={this._handleLogin}>
+            <div className="group">
+              <input type="text" className="mag-form" name="emailField" placeholder="Email" />
+              <span className="highlight"></span>
+              <span className="bar"></span>
+            </div>
+            <div className="group">
+              <input type="password" className="mag-form" name="passwordField" placeholder="Password" />
+              <span className="highlight"></span>
+              <span className="bar"></span>
+            </div>
+            <button type="submit" className="loginbut"><img src="../../images/tree-576846_1280.png" className="loginlog" /><span className="label loginarrow_box">LOG in</span></button>
+          </form>
+        </div>
+      </div>
+    )
+  }
 })
--- Application/src/Application.Web/src-client/scripts/components/register-component.js
+++ Application/src/Application.Web/src-client/scripts/components/register-component.js
@@ -1,45 +1,44 @@
 import React from 'react'
-import {ACTIONS} from '../actions.js'
-import {UserModel} from '../models/model-user.js'
+import { ACTIONS } from '../actions.js'
+import { UserModel } from '../models/model-user.js'
 
 export const RegisterComponent = React.createClass({
 
 
-  render: function(){
-    return(
-      <div className = "hero">
-
-      <div className = "auth-form register-box">
-        <form onSubmit={this._handleNewUser}>
-        <div className = "group">
-          <p>choose a username</p>
-          <input type = "text" className = "form-control" name = "usernameField" />
-                <span className="highlight"></span>
-                <span className="bar"></span>
-        </div>
-        <div className = "group">
-          <p>choose a unique password</p>
-          <input type = "password" className = "form-control" name = "passwordField" />
-          <span className="highlight"></span>
-          <span className="bar"></span>
-      </div>
-          <div className = "group">
-          <p>confirm password</p>
-          <input type = "password" className = "form-control" name = "passwordFieldConfirm" />
-          <span className="highlight"></span>
-          <span className="bar"></span>
-      </div>
+  render: function () {
+    return (
+      <div>        
+        <div className="auth-form register-box">
+          <form onSubmit={this._handleNewUser}>
+            <div className="group">
+              <p>choose a username</p>
+              <input type="text" className="form-control" name="usernameField" />
+              <span className="highlight"></span>
+              <span className="bar"></span>
+            </div>
+            <div className="group">
+              <p>choose a unique password</p>
+              <input type="password" className="form-control" name="passwordField" />
+              <span className="highlight"></span>
+              <span className="bar"></span>
+            </div>
+            <div className="group">
+              <p>confirm password</p>
+              <input type="password" className="form-control" name="passwordFieldConfirm" />
+              <span className="highlight"></span>
+              <span className="bar"></span>
+            </div>
             <button type="submit">Start Identifying</button>
 
-        </form>
+          </form>
 
+        </div>
       </div>
-    </div>
 
     )
   },
 
-  _handleNewUser: function(evt){
+  _handleNewUser: function (evt) {
     evt.preventDefault()
     let formEl = evt.target
     // if (formEl.usernameField.value !== formEl.passwordField.value){
@@ -49,10 +48,11 @@ export const RegisterComponent = React.createClass({
     let formValsObj = {
       email: formEl.usernameField.value,
       password: formEl.passwordField.value,
-      confirmPassword: formEl.passwordFieldConfirm.value}
+      confirmPassword: formEl.passwordFieldConfirm.value
+    }
 
 
     ACTIONS.registerNewUserM(formValsObj)
 
-}
+  }
 })
