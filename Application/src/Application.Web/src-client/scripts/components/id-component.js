ac1b1bb0cd4ef1517c1ea7eece3b6be8464fb9a4
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-29 09:53:31 -0400

    Clean up after merge conflict

--- Application/src/Application.Web/src-client/scripts/components/id-component.js
+++ Application/src/Application.Web/src-client/scripts/components/id-component.js
@@ -115,15 +115,9 @@ _renderConfidence: function () {
 
   render: function(){
     let answeredQuestions = this.props.answeredQuestions
-<<<<<<< HEAD
     let categorySelected = this.props.categorySelect
-    let numberOfanseredQuestions = answeredQuestions.length
-=======
     let numberOfansweredQuestions = answeredQuestions.length
->>>>>>> 4c7bdd55949ff815a78ac75eb237c9672fc07283
     let currentQuestion = this.props.currentQuestion
-    // let questionsInCategory = this.props.allProps[categorySelected]
-    // console.log(this.props.allProps)
 
     if (numberOfansweredQuestions === 0) {
       return this._renderCategoriesQuestion()
@@ -135,7 +129,7 @@ _renderConfidence: function () {
       else if (this.props.best.occurrences ===1 || this.props.filteredTrees === this.props.best.occurrences) {
       return this._renderConfidence()
     } else {
-    // ???? Something ain't right
+      // ???? Something ain't right
       return(
         <div className = 'bye'></div>
       )
