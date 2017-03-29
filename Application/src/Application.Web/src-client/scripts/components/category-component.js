dea7736fa379515c63943cef4783cd1e7522bbcf
Andrew Wyckoff <andrewrwyckoff@gmail.com>
2017-03-29 11:20:35 -0400

    update styles on browse view

--- Application/src/Application.Web/src-client/scripts/components/category-component.js
+++ Application/src/Application.Web/src-client/scripts/components/category-component.js
@@ -20,10 +20,10 @@ export const CategoryComponent = React.createClass({
         }
       }
     )
-    
+
     return (
       <div>
-        <h4>Categories</h4>
+        <h4 className="text-center">Categories</h4>
         {keyNamesJsx}
       </div>
     )
