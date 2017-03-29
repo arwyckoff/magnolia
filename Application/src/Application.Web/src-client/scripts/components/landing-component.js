2551a0e355d9a1d292dc99039f5183682e5ab759
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-28 22:30:31 -0400

    Simplify landing page

--- Application/src/Application.Web/src-client/scripts/components/landing-component.js
+++ Application/src/Application.Web/src-client/scripts/components/landing-component.js
@@ -1,15 +1,13 @@
 import React from 'react'
-
-import {STORE} from '../store.js'
-import {ACTIONS} from '../actions.js'
+import { STORE } from '../store.js'
+import { ACTIONS } from '../actions.js'
 
 export const LandingComponent = React.createClass({
-
-   render: function(){
+  render: function () {
     return (
-      <div className = "landing-container">
-        <h1>Magnolia Tree ID</h1>
+      <div className="landing-container">
+        <h1>M<span className="smallcaps">AGNOLIA</span> T<span className="smallcaps">REE</span> ID</h1>
       </div>
     )
-   }
+  }
 })
