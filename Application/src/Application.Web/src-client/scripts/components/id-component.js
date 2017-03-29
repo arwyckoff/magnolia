7ba53c3e51c3579b4c429b7cbee8f61fe0388ae5
Alexandre Guerra Marcondes <alex@marcondesian.net>
2017-03-29 11:14:02 -0400

    Add category images to ID view

--- Application/src/Application.Web/src-client/scripts/components/id-component.js
+++ Application/src/Application.Web/src-client/scripts/components/id-component.js
@@ -1,119 +1,118 @@
-import React from 'react';
-import {STORE} from '../store.js';
-import {ACTIONS} from '../actions.js';
-import {BROWSE_ACTIONS} from '../browse_actions.js'
-import {ID_ACTIONS} from '../id-actions.js'
-import  _getFilteredTrees  from "../utils/getFilteredTrees";
-
+import React from 'react'
+import { STORE } from '../store.js'
+import { ACTIONS } from '../actions.js'
+import { BROWSE_ACTIONS } from '../browse_actions.js'
+import { ID_ACTIONS } from '../id-actions.js'
+import _getFilteredTrees from "../utils/getFilteredTrees"
+import CATEGORY_IMAGES from '../utils/categoryImages.js'
 import _getNextCategoryQuestion from "../utils/getNextCategoryQuestion.js"
 
 export const IdComponent = React.createClass({
-  getInitialState: function(){
+  getInitialState: function () {
     return STORE.getStoreData()
   },
 
-  componentWillMount: function(){
+  componentWillMount: function () {
     ID_ACTIONS.resetIDProps()
   },
 
-  _makeQuestionComponents: function(categories){
+  _makeQuestionComponents: function (categories) {
     let keyNameJsx = Object.keys(categories).map(
       (smod, i) => {
-        return <QuestionItem questionData = {smod} key = {i} allProps = {this.props}/>
+        return <QuestionItem questionData={smod} key={i} allProps={this.props} />
       }
     )
     return keyNameJsx
   },
 
-  _makeAnswersComponent: function(answersArray, questionObj){
+  _makeAnswersComponent: function (answersArray, questionObj) {
     let answersJsx = answersArray.map(
       (smod, i) => {
-        return <PhaseOneQuestionItem questionObjData = {questionObj} questionData = {smod} key = {i} allProps = {this.props}/>
+        return <PhaseOneQuestionItem questionObjData={questionObj} questionData={smod} key={i} allProps={this.props} />
       }
     )
     return answersJsx
   },
 
-  _makePartOneComponents: function(questions){
+  _makePartOneComponents: function (questions) {
     let phaseOneQuestionJSX = questions.map(
       (smod, i) => {
-        return   <PhaseOneQuestionItem questionData = {smod} key = {i} />
+        return <PhaseOneQuestionItem questionData={smod} key={i} />
       }
     )
     return phaseOneQuestionJSX
   },
 
-  _makePartTwoComponents: function(statesArray){
+  _makePartTwoComponents: function (statesArray) {
     let stateJsx = statesArray.map(
       (keyName, i) => {
         let futurefiltChars = [...this.props.filterChars]
         futurefiltChars.push(keyName.code)
         let resultCount = _getFilteredTrees(futurefiltChars, this.props.filteredTrees).length
-        return <PartTwoItem partTwoData={keyName} results={resultCount} key = {i}/>
+        return <PartTwoItem partTwoData={keyName} results={resultCount} key={i} />
       }
     )
     return stateJsx
   },
 
-  _handleidontknow: function(evt){
+  _handleidontknow: function (evt) {
     let catClicked = evt.currentTarget.dataset.cat
     let currentChar = evt.currentTarget.dataset.ch
     ID_ACTIONS.getNextBest(evt.currentTarget.dataset.cat, evt.currentTarget.dataset.ch)
   },
   _renderPhaseOne: function () {
-  let currentCatSelect = this.state.categorySelect
-  let phaseOneQuestions = this.props.allQuestions
-  let catQuestionTrackerEl = this.props.catQuestionTracker[currentCatSelect]
-  let catCodeArray = catQuestionTrackerEl["codeArray"]
-  let questionObjArray = phaseOneQuestions[currentCatSelect]
-  let nextQuestion = _getNextCategoryQuestion(questionObjArray, catCodeArray, this.props.answeredQuestions)
-  let questionText = nextQuestion["question"]
-  let answersArray = nextQuestion["answers"]
-  let answerEls = this._makeAnswersComponent(answersArray, nextQuestion)
-  return (
-    <div className = "question-box">
-      {questionText}
-      {answerEls}
-    </div>
-  )
-},
+    let currentCatSelect = this.state.categorySelect
+    let phaseOneQuestions = this.props.allQuestions
+    let catQuestionTrackerEl = this.props.catQuestionTracker[currentCatSelect]
+    let catCodeArray = catQuestionTrackerEl["codeArray"]
+    let questionObjArray = phaseOneQuestions[currentCatSelect]
+    let nextQuestion = _getNextCategoryQuestion(questionObjArray, catCodeArray, this.props.answeredQuestions)
+    let questionText = nextQuestion["question"]
+    let answersArray = nextQuestion["answers"]
+    let answerEls = this._makeAnswersComponent(answersArray, nextQuestion)
+    return (
+      <div className="question-box">
+        {questionText}
+        {answerEls}
+      </div>
+    )
+  },
 
-_renderCategoriesQuestion: function () {
-  let questionStuff = this._makeQuestionComponents(this.props.categories)
-  return (
-    <div className = "question-box">
-      <h4 className = "id-view-header">Choose part of plant to identify</h4>
-      {questionStuff}
+  _renderCategoriesQuestion: function () {
+    let questionStuff = this._makeQuestionComponents(this.props.categories)
+    return (
+      <div className="question-box">
+        <h4 className="id-view-header">Choose part of plant to identify</h4>
+        {questionStuff}
 
-    </div>
-  )
+      </div>
+    )
 
-},
+  },
 
-_renderPhaseTwo: function () {
-  let stateStuff = this.props.best.characteristic.states
-  let charStuff = this._makePartTwoComponents(stateStuff)
-  return (
-    <div className = "question-box">
-    <h4>Choose best answer for {this.props.best.characteristic.characteristic}</h4>
-      {charStuff}
-      <div className = "question-card question-center" data-ch= {this.props.best.characteristic.characteristic} data-cat={this.props.categorySelect} onClick = {this._handleidontknow}>
+  _renderPhaseTwo: function () {
+    let stateStuff = this.props.best.characteristic.states
+    let charStuff = this._makePartTwoComponents(stateStuff)
+    return (
+      <div className="question-box">
+        <h4>Choose best answer for {this.props.best.characteristic.characteristic}</h4>
+        {charStuff}
+        <div className="question-card question-center" data-ch={this.props.best.characteristic.characteristic} data-cat={this.props.categorySelect} onClick={this._handleidontknow}>
           <p>I don't know/skip</p>
         </div>
-    </div>
-  )
-},
+      </div>
+    )
+  },
 
-_renderConfidence: function () {
-  return (
-    <div className = "question-box">
-      <h4>We believe your tree is below</h4>
-    </div>
-  )
-},
+  _renderConfidence: function () {
+    return (
+      <div className="question-box">
+        <h4>We believe your tree is below</h4>
+      </div>
+    )
+  },
 
-
-  render: function(){
+  render: function () {
     let answeredQuestions = this.props.answeredQuestions
     let categorySelected = this.props.categorySelect
     let numberOfansweredQuestions = answeredQuestions.length
@@ -121,17 +120,18 @@ _renderConfidence: function () {
 
     if (numberOfansweredQuestions === 0) {
       return this._renderCategoriesQuestion()
-    } else if (numberOfansweredQuestions > 0 && numberOfansweredQuestions < 2 ){
-            console.log('test')
+    } else if (numberOfansweredQuestions > 0 && numberOfansweredQuestions < 2) {
+      console.log('test')
       return this._renderPhaseOne()
-    } else if (numberOfansweredQuestions >= 2 && this.props.best.characteristic !== null  && this.props.best.occurrences >1){
-      return this._renderPhaseTwo()}
-      else if (this.props.best.occurrences ===1 || this.props.filteredTrees === this.props.best.occurrences) {
+    } else if (numberOfansweredQuestions >= 2 && this.props.best.characteristic !== null && this.props.best.occurrences > 1) {
+      return this._renderPhaseTwo()
+    }
+    else if (this.props.best.occurrences === 1 || this.props.filteredTrees === this.props.best.occurrences) {
       return this._renderConfidence()
     } else {
       // ???? Something ain't right
-      return(
-        <div className = 'bye'></div>
+      return (
+        <div className='bye'></div>
       )
     }
   }
@@ -140,33 +140,33 @@ _renderConfidence: function () {
 
 export const QuestionItem = React.createClass({
 
-  _handleQuesSelect: function(evt){
+  _handleQuesSelect: function (evt) {
     evt.preventDefault()
     let catClicked = evt.currentTarget.dataset.cat
     ID_ACTIONS.firstQuestionAction(evt.currentTarget.dataset.cat)
   },
 
-  render: function (){
+  render: function () {
     let self = this
-    if (this.props.questionData !== 'GENERAL'){
-    return (
-      <div className = "question-card" data-cat={this.props.questionData} onClick = {this._handleQuesSelect}>
-        <p>{this.props.questionData}</p>
-        <img src ="http://placehold.it/200"/>
-      </div>
-    )
-  }
-  else {
-    return(
-      <div className = 'bye'></div>
-    )
-  }
+    if (this.props.questionData !== 'GENERAL') {
+      return (
+        <div className="question-card" data-cat={this.props.questionData} onClick={this._handleQuesSelect}>
+          <p>{this.props.questionData}</p>
+          <img src={CATEGORY_IMAGES[this.props.questionData]} />
+        </div>
+      )
+    }
+    else {
+      return (
+        <div className='bye'></div>
+      )
+    }
   }
 })
 
 export const PhaseOneQuestionItem = React.createClass({
 
-  _handlePhaseOneSelect:function(evt){
+  _handlePhaseOneSelect: function (evt) {
     let answerCode = evt.currentTarget.dataset.code
     let answerApply = evt.currentTarget.dataset.apply
     let questionText = evt.currentTarget.dataset.question
@@ -176,7 +176,7 @@ export const PhaseOneQuestionItem = React.createClass({
     ID_ACTIONS.answerPhaseOneAction(answerCat, answerCode, answerApply, questionText, questionChar)
   },
 
-  render: function(){
+  render: function () {
     let catAnswers = this.props.questionData["answer"]
     let catCode = this.props.questionData["code"]
     let catApply = this.props.questionData["apply"]
@@ -188,7 +188,7 @@ export const PhaseOneQuestionItem = React.createClass({
 
     return (
 
-      <div data-code={catCode} data-apply={catApply} data-question={question} data-characteristic = {characteristicA} onClick={this._handlePhaseOneSelect} className = "question-card hvr-grow">
+      <div data-code={catCode} data-apply={catApply} data-question={question} data-characteristic={characteristicA} onClick={this._handlePhaseOneSelect} className="question-card hvr-grow">
         {catAnswers}
       </div>
 
@@ -198,7 +198,7 @@ export const PhaseOneQuestionItem = React.createClass({
 
 export const PartTwoItem = React.createClass({
 
-  _handleQuesSelect: function(evt){
+  _handleQuesSelect: function (evt) {
     evt.preventDefault()
     let filterChar = evt.currentTarget.dataset.id
     let catClicked = evt.currentTarget.dataset.cat
@@ -206,24 +206,26 @@ export const PartTwoItem = React.createClass({
     ID_ACTIONS.updateQuestionInfo(evt.currentTarget.dataset.cat, evt.currentTarget.dataset.id, evt.currentTarget.dataset.ch)
   },
 
-  render: function(){
-  if (this.props.results === 1){
-    return(
-      <div className = "question-card" data-ch= {this.props.partTwoData.characteristic} data-cat={this.props.categorySelect} data-id = {this.props.partTwoData.code} onClick = {this._handleQuesSelect}>
+  render: function () {
+    if (this.props.results === 1) {
+      return (
+        <div className="question-card" data-ch={this.props.partTwoData.characteristic} data-cat={this.props.categorySelect} data-id={this.props.partTwoData.code} onClick={this._handleQuesSelect}>
           <p>{this.props.partTwoData.state}</p>
         </div>
-    )
-  }
-   if(this.props.results>1){
-    return(
-        <div className = "question-card" data-ch= {this.props.partTwoData.characteristic} data-cat={this.props.categorySelect} data-id = {this.props.partTwoData.code} onClick = {this._handleQuesSelect}>
-            <p>{this.props.partTwoData.state}</p>
-          </div>
       )
+    }
+    if (this.props.results > 1) {
+      return (
+        <div className="question-card" data-ch={this.props.partTwoData.characteristic} data-cat={this.props.categorySelect} data-id={this.props.partTwoData.code} onClick={this._handleQuesSelect}>
+          <p>{this.props.partTwoData.state}</p>
+        </div>
+      )
+    }
+    if (this.props.results === 0) {
+      return (
+        <div className='bye'></div>
+      )
+    }
   }
-  if (this.props.results ===0){return   (
-      <div className = 'bye'></div>
-        )
-}}
 
 })
